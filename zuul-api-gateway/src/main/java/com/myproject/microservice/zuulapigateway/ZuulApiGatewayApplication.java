b0e9666d37b3b7d7a9b6b62295094ed8f3ee9c5e
Kitinat Chantee <kitinat@hotmail.com>
2020-01-24 17:23:38 +0700

    Request api gateway

--- zuul-api-gateway/src/main/java/com/myproject/microservice/zuulapigateway/ZuulApiGatewayApplication.java
+++ zuul-api-gateway/src/main/java/com/myproject/microservice/zuulapigateway/ZuulApiGatewayApplication.java
@@ -1,15 +1,17 @@
-package com.myproject.microservice.currencyexchangeservice;
+package com.myproject.microservice.zuulapigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 @EnableDiscoveryClient
-public class CurrencyExchangeServiceApplication {
+public class ZuulApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
+		SpringApplication.run(ZuulApiGatewayApplication.class, args);
 	}
 
 }
